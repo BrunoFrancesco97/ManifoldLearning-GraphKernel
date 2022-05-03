08b89a63c3b4287428c614fd481278e3743d43ad
BrunoFrancesco97 <francescobruno398@gmail.com>
2022-05-03 17:58:06 +0200

    Update

---
+++ utils.py
@@ -0,0 +1,29 @@
+import csv
+from grakel import Graph
+
+def readFromFile(numbers,labelsFile,folder):
+    files = list()
+    graphs = list()
+    labels = list()
+    for el in numbers:
+        string = "datasets/"+folder+"/graph"+el+".csv" 
+        files.append(string)
+    for el in files:
+        file = open(el, "r")
+        csvreader = csv.reader(file)
+        rows = []
+        for row in csvreader:
+                rows.append(row)
+
+        matrix_size = len(rows)
+        edges = list()
+        for i in range(0,matrix_size):
+            for j in range(0,matrix_size):
+                if(rows[i][j] == "1"):
+                    edges.append((i,j))
+        graphs.append(Graph(edges))
+    file = open(labelsFile, "r")
+    csvreader = csv.reader(file)
+    for el in csvreader:
+        labels.append(int(el[0]))
+    return (graphs,labels)
