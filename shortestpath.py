be0f963eccf7b19a09829dd375b0ab4a1d8b5d74
Francesco <56157402+BrunoFrancesco97@users.noreply.github.com>
2022-05-10 21:28:03 +0200

    Update shortestpath.py

--- shortestpath.py
+++ shortestpath.py
@@ -1,18 +1,18 @@
-
 import numpy as np
 from numpy.linalg import norm
 
+
 class ShortestPathKernel():
     def FloydWarshall(self, graph):
-        dist = graph
-        dist[dist == 0] = float('inf')
-        np.fill_diagonal(dist, 0)
+        shortpath = graph
+        shortpath[shortpath == 0] = float('inf')
+        np.fill_diagonal(shortpath, 0)
         dim = graph.shape[0]
         for k in range(dim):
             for i in range(dim):
                 for j in range(dim):
-                    dist[i,j] = min(dist[i,j], dist[i,k] + dist[k,j])
-        return dist
+                    shortpath[i,j] = min(shortpath[i,j], shortpath[i,k] + shortpath[k,j])
+        return shortpath
     
     def shortestPaths(self, adjMatrices):
         shortestPaths = []
@@ -21,23 +21,22 @@ class ShortestPathKernel():
         return shortestPaths
 
 
-    def getFrequencyVector(self, paths, delta):
-        freq = np.empty([delta+1, 1])
+    def calcFrequencyVector(self, graph, delta):
+        frequency = np.empty([delta+1, 1])
         for i in range(delta+1):
-            freq[i] = np.sum(paths == i)
-        res = freq/norm(freq)
-        return res
+            frequency[i] = np.sum(graph == i)
+        return frequency/norm(frequency)
       
-    def deltaKernel(self, paths1, paths2):
-        delta = int(np.maximum(np.max(paths1), np.max(paths2)))
-        freq1 = self.getFrequencyVector(paths1, delta)
-        freq2 = self.getFrequencyVector(paths1, delta)
-        return  np.dot(np.transpose(freq1), freq2)[0]
+    def deltaKernel(self, shortestPathGraph1, shortestPathGraph2):
+        delta = int(np.maximum(np.max(shortestPathGraph1), np.max(shortestPathGraph2)))
+        frequencyVector1 = self.calcFrequencyVector(shortestPathGraph1, delta)
+        frequencyVector2 = self.calcFrequencyVector(shortestPathGraph2, delta)
+        return  np.dot(np.transpose(frequencyVector1), frequencyVector2)[0]
     
-    def getSimilarities(self, shortestPathGraphs):
-        n = len(shortestPathGraphs)
+    def getSimilarities(self, shortestPathgraphs):
+        n = len(shortestPathgraphs)
         similarities = np.zeros((n, n))
         for i in range(n):
             for j in range(n):
-                similarities[i,j] = self.deltaKernel(shortestPathGraphs[i], shortestPathGraphs[j])
+                similarities[i,j] = self.deltaKernel(shortestPathgraphs[i], shortestPathgraphs[j])
         return similarities
